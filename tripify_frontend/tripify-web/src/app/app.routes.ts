eae4336e642aae6d1638c4b9f6132b69deb002e4
Nandana_K_Saji <ksajinandana@gmail.com>
2026-01-03 10:08:26 +0530

    changes

--- tripify_frontend/tripify-web/src/app/app.routes.ts
+++ tripify_frontend/tripify-web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePage },
+];
